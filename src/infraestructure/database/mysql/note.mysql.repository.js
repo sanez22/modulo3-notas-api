0e0e8ef9ba52c226475c3a71552db60fafbcfd55
sanez22 <saulanez@gmail.com>
2026-04-26 18:37:37 -0400

    tarea: incorporación de JWT en los metodos de delete y updatE  MYSQL

--- src/infraestructure/database/mysql/note.mysql.repository.js
+++ src/infraestructure/database/mysql/note.mysql.repository.js
@@ -15,4 +15,26 @@ export default class NoteMySQLRepository {
     async findByUserId(userId) {
         return await NoteModel.findAll({ where: { userId } });
     }
+
+    async findById(id) {
+        const note = await NoteModel.findByPk(id);
+        return note ? note.toJSON(): null;
+    }
+
+    async update(id, data) {
+        const note = await NoteModel.findByPk(id);
+        if (!note) return null;
+
+        await note.update(data);
+        return note.toJSON();
+    }
+
+    async delete(id) {
+
+        const note = await NoteModel.findByPk(id);
+        if (!note) return null;
+        
+        await note.destroy();
+        return note.toJSON();
+    }
 }
